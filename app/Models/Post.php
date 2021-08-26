1195c9e877d514cca204efec95cb447a4c3c3ce5
Matiasss11 <kovalski.matias@gmail.com>
2021-08-26 07:53:25 -0300

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function getExcerptAttribute()
+    {
+        return substr($this->content, 0, 120);
+    }
+
+    public function getPublishedAtAttribute()
+    {
+        return $this->created_at->format('d-m-Y');
+    }
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
